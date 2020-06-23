9255f1dab3c7c1b0ea5af0bf80702866af1caae6
Vova_Ivanitskyi <vovanhuk2003@gmail.com>
2020-06-23 20:22:48 +0300

    Euler 9 is completed

--- Euler 9/Program.cs
+++ Euler 9/Program.cs
@@ -6,7 +6,20 @@ namespace Euler_9
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //int a;
+            //int b;
+            double c;
+            for (int a = 1; a < 300; a++)
+                for(int b = 2; b < 400; b++)
+                {
+                    if(Math.Sqrt(a*a+b*b)%1 == 0)
+                    {
+                        c = Math.Sqrt((a * a) + (b * b));
+                        if((a+b+c)== 1000)
+                            Console.WriteLine($"a = {a}, b = {b}, c = {c}");
+                        // a = 200, b = 375, c = 425
+                    }
+                }
         }
     }
 }
